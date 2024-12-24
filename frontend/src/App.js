3a20054ff1a89f20c8577e4a778f31c848915d58
Ronak Shah <ron94.s@gmail.com>
2024-12-23 20:42:57 -0500

    Setup boilerplate backend and frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CDBurningAnimation from './components/CDBurningAnimation';
+
+function App() {
+  return (
+    <div className="app">
+      <CDBurningAnimation />
+    </div>
+  );
+}
+
+export default App;
