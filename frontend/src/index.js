c04cce50a6bcd0ec7689e4f448fb2e7e0d12d6ed
Ronak Shah <ron94.s@gmail.com>
2024-12-25 12:49:29 -0500

    Dummy commit for easier diff

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render([
+//   <div>
+//           <script src="./script.js" type="module"></script>
+//           <section id="profile">
+//               <h2>Logged in as <span id="displayName"></span></h2>
+//               <span id="avatar"></span>
+//               <ul>
+//                   <li key="id">User ID: <span id="id"></span></li>
+//                   <li key="email">Email: <span id="email"></span></li>
+//                   <li key="uri">Spotify URI: <a id="uri" href="#"></a></li>
+//                   <li key="link">Link: <a id="url" href="#"></a></li>
+//                   <li key="img">Profile Image: <span id="imgUrl"></span></li>
+//               </ul>
+//           </section> 
+//           </div>
+// ]);
 
